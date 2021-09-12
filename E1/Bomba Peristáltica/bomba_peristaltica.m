f6f9f8b023e5273748c898e747e58ad14a5615fc
Ronald Tighe <ratighe@uc.cl>
2021-09-12 19:04:53 -0300

    codigo grafico bombas

--- E1/Bomba Peristáltica/bomba_peristaltica.m
+++ E1/Bomba Peristáltica/bomba_peristaltica.m
@@ -1,6 +1,13 @@
-%% Bomba LabV1
+
 rpm_labv1 = out.LabV1(:,1);
 f_labv1 = out.LabV1(:,2);
+rpm_IF3 = out.IF3(:,1);
+f_IF3 = out.IF3(:,2);
+rpm_LabF1 = out.LabF1(:,1);
+f_LabF1 = out.LabF1(:,2);
+
+grafico = tiledlayout(3,1)
+nexttile
 plot(rpm_labv1,f_labv1)
 grid on
 xlabel('RPM')
@@ -8,9 +15,8 @@ ylabel('Flujo (mL/min)')
 legend('Bomba LabV1')
 xlim([0 150])
 ylim([0 2])
-%% Bomba IF3 (refrigerante)
-rpm_IF3 = out.IF3(:,1);
-f_IF3 = out.IF3(:,2);
+
+nexttile
 plot(rpm_IF3,f_IF3)
 grid on
 xlabel('RPM')
@@ -19,7 +25,7 @@ legend('Bomba IF3')
 xlim([0 350])
 ylim([0 3500])
 
-%% Bomba LabF1
+nexttile
 rpm_LabF1 = out.LabF1(:,1);
 f_LabF1 = out.LabF1(:,2);
 plot(rpm_LabF1,f_LabF1)
