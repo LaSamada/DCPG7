6790a205ed61d4f0f4c5534c33a9c2453cbc6af4
Sebasthian Schnettler <ssschnettler@uc.cl>
2021-09-12 21:05:55 -0300

    Nuevos gráficos de T°

--- E1/s_temperatura.m
+++ E1/s_temperatura.m
@@ -9,6 +9,8 @@ nexttile
 plot(tiempo,YSL_PRO,'c')
 xlabel('Tiempo (s)')
 ylabel('Temperatura (C°)')
+ylim([35,40])
+xlim([4,6])
 hold on
 plot(tiempo,Step,'k')
 legend('YSL PRO','Referencia')
@@ -20,6 +22,8 @@ plot(tiempo,FL_TF,'g')
 xlabel('Tiempo (s)')
 ylabel('Temperatura (C°)')
 hold on
+ylim([35,40])
+xlim([4,6])
 plot(tiempo,Step,'k')
 legend('FL TF','Referencia')
 grid on
@@ -30,6 +34,8 @@ plot(tiempo,analog,'m')
 xlabel('Tiempo (s)')
 ylabel('Temperatura (C°)')
 hold on
+ylim([35,40])
+xlim([4,6])
 plot(tiempo,Step,'k')
 legend('Analog Devices','Referencia')
 grid on
